64206577247669a16934c5198fb608f5a5f04f9d
Bernardo <bernardopc60@gmail.com>
2025-02-21 23:16:29 -0600

    Se actualizo la carpeta img/ con los nuevas imagenes y se creo el archivo get_productos_vigentes.php

---
+++ practicas/p08/get_productos_vigentes.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+    <?php
+    if(isset($_GET['elim'])) {
+        $elim = $_GET['elim'];
+
+        // Validar que 'elim' solo pueda ser 0 o 1
+        if ($elim !== '0' && $elim !== '1') {
+            die('<p class="alert alert-danger">Error: El parámetro "elim" solo puede ser 0 o 1.</p>');
+        }
+        $elim = intval($elim); // Convertimos a número entero seguro
+    } else {
+        die('<p class="alert alert-danger">Parámetro "elim" no detectado...</p>');
+    }
+
+    if ($elim === 0) { // Solo mostrar productos NO eliminados
+        /** SE CREA EL OBJETO DE CONEXION */
+        @$link = new mysqli('localhost', 'root', '7*Vl*zwyPeGlNLTm', 'marketzone');
+        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+
+        /** Comprobar la conexión */
+        if ($link->connect_errno) {
+            die('<p class="alert alert-danger">❌ Falló la conexión: ' . $link->connect_error . '</p>');
+        }
+
+        /** Obtener los productos con eliminado = 0 */
+        $productos = [];
+        if ($result = $link->query("SELECT * FROM productos WHERE eliminado = 0")) {
+            $productos = $result->fetch_all(MYSQLI_ASSOC);
+            $result->free();
+        }
+        $link->close();
+    }
+    ?>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Productos XHTML</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <h3 class="mt-4">PRODUCTOS</h3>
+        <br/>
+        <?php if(!empty($productos)) : ?>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Unidades</th>
+                        <th scope="col">Detalles</th>
+                        <th scope="col">Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($productos as $producto) : ?>
+                    <tr>
+                        <th scope="row"><?= htmlspecialchars($producto['id']) ?></th>
+                        <td><?= htmlspecialchars($producto['nombre']) ?></td>
+                        <td><?= htmlspecialchars($producto['marca']) ?></td>
+                        <td><?= htmlspecialchars($producto['modelo']) ?></td>
+                        <td><?= htmlspecialchars($producto['precio']) ?></td>
+                        <td><?= htmlspecialchars($producto['unidades']) ?></td>
+                        <td><?= htmlspecialchars($producto['detalles']) ?></td>
+                        <td><img src="<?= htmlspecialchars($producto['imagen']) ?>" width="100"></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p class="alert alert-warning">No hay productos disponibles.</p>
+        <?php endif; ?>
+    </body>
+</html>
